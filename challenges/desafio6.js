a8ecdc4e5f8609bdd6388d0952170092c68e4b06
EliasGalindo0 <eliasgalindo@gmail.com>
2022-09-08 22:55:30 -0300

    feat: challenge6

---
+++ challenges/desafio6.js
@@ -0,0 +1,8 @@
+// 6 - Retorne o nome e as curtidas dos lanches que tiveram curtidas maiores que 10 e menores que 100
+
+db.produtos.find({
+  curtidas: {
+    $gte: 10,
+    $lt: 100,
+  },
+}, { nome: 1, curtidas: 1, _id: 0 });
