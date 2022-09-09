582e8a073f2b4e8979e7ccaac402b0aa15189c20
EliasGalindo0 <eliasgalindo@gmail.com>
2022-09-09 17:47:59 -0300

    feat: challenge10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -4,10 +4,7 @@ db.produtos.find({
   valoresNutricionais: {
     $elemMatch: {
       tipo: "proteínas",
-      percentual: {
-        $gte: 30,
-        $lte: 40,
-      },
+      percentual: { $gte: 30, $lte: 40 },
     },
   },
 }, { nome: 1, _id: 0 });
